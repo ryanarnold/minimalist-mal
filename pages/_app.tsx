6475dea763a873c3c6c78b1aff2ad5b7126d0e4f
Ryan <ryan@geekful.dev>
2022-05-19 21:17:24 +0800

    Fix lint issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/react-in-jsx-scope
   return <Component {...pageProps} />;
 }
 
